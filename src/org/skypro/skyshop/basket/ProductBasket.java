4e502045887ac093fef6355fddb6dafb21e1006a
asonovserg <ason11s@bk.ru>
2025-05-04 19:47:54 +0500

    Home work done

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,20 +1,53 @@
 package org.skypro.skyshop.basket;
-import java.util.List;
-import java.util.ArrayList;
 import org.skypro.skyshop.product.Product;
 
 public class ProductBasket {
-    private List<Product> products = new ArrayList<>();
+    private final Product [] products = new Product[5];
+    private int size = 0;
     public void addProduct(Product product) {
-        products.add(product);
+        if(size >= products.length){
+            System.out.println("Невозможно добавить продукт");
+            return;
+        }
+        products[size] = product;
+        size++;
     }
 
-    public int getTotalPrice(){
-        int totalPrice = 0;
-        for(Product product :products){
-            totalPrice += product.getPrice();
+    public int getTotalPrice() {
+        int total = 0;
+        for (int i = 0; i < size; i++) {
+            if (products[i] != null) {
+                total += products[i].getPrice();
+            }
         }
-        return totalPrice;
+        return total;
+    }
 
+    public void printBasket() {
+        if (size == 0) {
+            System.out.println("В корзине пусто");
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            if (products[i] != null) {
+                System.out.printf("%s : %d%n", products[i].getName(), products[i].getPrice());
+            }
+        }
+        System.out.println("Итого: " + getTotalPrice());
+    }
+
+    public boolean containsProduct(String name) {
+        for (int i = 0; i < size; i++) {
+            if (products[i] != null && products[i].getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void clearBasket() {
+        for (int i = 0; i < size; i++) {
+            products[i] = null;
+        }
+        size = 0;
     }
 }
